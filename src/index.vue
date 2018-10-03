ab449205f2cb887b1c20e9f74d056a10568aecec
dyy8897 <dingyun8897@yahoo.com.cn>
2018-10-03 20:29:18 +0800

    tabbar

--- src/index.vue
+++ src/index.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="wrapper">
-    <image :src="logo" class="logo" />
-    <text class="greeting">The environment is ready!</text>
-    <router-view/>
+  <div>
+    <div class="tabbar">
+      <div class="tab active" :style="{ left: activeTab * 150 + 'px'}"></div>
+      <div v-for="(tab, i) in tabs" :key="i" class="tab" @click="activeTab = i">
+        <image class="icon" :src="tab.icon"></image>
+        <text class="title">{{tab.title}}</text>
+      </div>
+    </div>
+    <div class="tab-panels" :style="{ left: activeTab * -750 + 'px'}">
+      <div class="panel" v-for="(panel, pi) in panels" :key="pi">
+        <text class="content">{{panel.content}}</text>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,30 +20,76 @@ export default {
   name: 'App',
   data () {
     return {
-      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png'
+      activeTab: 0,
+      tabs: [{
+        title: '用户',
+        icon: '//gw.alicdn.com/tfs/TB19YESOVXXXXaNaXXXXXXXXXXX-45-45.png'
+      }, {
+        title: '病案',
+        icon: '//gw.alicdn.com/tfs/TB1I2E9OVXXXXbFXVXXXXXXXXXX-45-45.png'
+      }, {
+        title: '字典',
+        icon: '//gw.alicdn.com/tfs/TB1gUhyPXXXXXX5XXXXXXXXXXXX-45-45.png'
+      }, {
+        title: 'DRG',
+        icon: '//img.alicdn.com/tfs/TB1D4RzQFXXXXcoXpXXXXXXXXXX-45-45.png'
+      }, {
+        title: '区块',
+        icon: '//gw.alicdn.com/tfs/TB1N1.6OVXXXXXqaXXXXXXXXXXX-45-45.png'
+      }]
+    }
+  },
+  computed: {
+    panels () {
+      return this.tabs.map(tab => ({ content: tab.title }))
     }
   }
 }
 </script>
 
 <style scoped>
-  .wrapper {
+  .tabbar {
+    flex-direction: row;
+    background-color: #FF4563;
+  }
+  .tab {
+    height: 120px;
+    width: 150px;
     justify-content: center;
     align-items: center;
   }
-  .logo {
-    width: 424px;
-    height: 200px;
+  .active {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgb(218, 59, 85);
+    transition: left 0.2s ease-in-out;
   }
-  .greeting {
-    text-align: center;
-    margin-top: 70px;
-    font-size: 50px;
-    color: #41B883;
+  .icon {
+    width: 45px;
+    height: 45px;
   }
-  .message {
-    margin: 30px;
-    font-size: 32px;
-    color: #727272;
+  .title {
+    font-size: 28px;
+    color: #FFF;
+    margin-top: 10px;
+  }
+  .tab-panels {
+    position: relative;
+    width: 3750;
+    flex: 1;
+    flex-direction: row;
+    align-items: stretch;
+    background-color: #F5F5F5;
+    transition: left 0.2s ease-in-out;
+  }
+  .panel {
+    width: 750px;
+    justify-content: center;
+    align-items: center;
+  }
+  .content {
+    font-size: 100px;
+    color: #707070;
   }
 </style>
