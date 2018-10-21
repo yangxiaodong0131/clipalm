4750ad7cedf86a74a843bdb6d459d717ca58a2ef
duanzhichao <duanzhichao2008@163.com>
2018-10-21 20:03:48 +0800

    自定义查询测试

--- src/components/stat/Query.vue
+++ src/components/stat/Query.vue
@@ -1,67 +1,56 @@
 <template>
-  <div class="panel">
-    <wxc-simple-flow :list="testData" :themeColor="themeColor"></wxc-simple-flow>
+  <div class="container">
+    <div class="demo">
+      <text class="value-text">基础输入框</text>
+      <wxc-searchbar ref="wxc-searchbar"
+                     @wxcSearchbarCancelClicked="wxcSearchbarCancelClicked"
+                     @wxcSearchbarInputReturned="wxcSearchbarInputReturned"
+                     @wxcSearchbarInputOnInput="wxcSearchbarInputOnInput"
+                     @wxcSearchbarCloseClicked="wxcSearchbarCloseClicked"
+                     @wxcSearchbarInputOnFocus="wxcSearchbarInputOnFocus"
+                     @wxcSearchbarInputOnBlur="wxcSearchbarInputOnBlur"></wxc-searchbar>
+      <text class="value-text">{{value}}</text>
+    </div>
+    <div class="demo demo1">
+      <text class="value-text">输入框禁用</text>
+      <wxc-searchbar disabled="true" @wxcSearchbarInputDisabledClicked="searchbarInputDisabledOnclick"></wxc-searchbar>
+    </div>
+    <div class="demo demo1">
+      <text class="value-text">带有目的地模式的输入框</text>
+      <wxc-searchbar disabled="true"
+                     placeholder="搜索目的地"
+                     mod="hasDep"
+                     dep-name="杭州出发"
+                     @wxcSearchbarDepChooseClicked="wxcSearchbarDepChooseClicked"
+                     @wxcSearchbarInputDisabledClicked="wxcSearchbarInputDisabledClicked"></wxc-searchbar>
+    </div>
   </div>
 </template>
 <script>
-import { WxcSimpleFlow } from 'weex-ui'
-
-export default {
-  components: { WxcSimpleFlow },
-  data: () => ({
-    themeColor: {
-      lineColor: '#bf280b',
-      pointInnerColor: '#b95048',
-      pointBorderColor: '#bf280b',
-      highlightTitleColor: '#bf280b',
-      highlightPointInnerColor: '#bf280b',
-      highlightPointBorderColor: '#d46262'
-    },
-    testData: [
-      {
-        'date': '2017-05-24 21:10:29',
-        'desc': '',
-        'highlight': true,
-        'title': '方案已确认'
-      },
-      {
-        'date': '2017-05-24 19:54:28',
-        'desc': '',
-        'title': '方案已更新'
-      },
-      {
-        'date': '2017-05-24 19:50:21',
-        'desc': '您以确定了方案',
-        'title': '方案已上传'
-      },
-      {
-        'date': '2017-05-24 19:49:03',
-        'desc': '商家会在2个工作小时内电话或旺旺联系您',
-        'title': '商家已接单'
-      }
-    ]
-  })
-}
+import { WxcSearchbar } from 'weex-ui'
+ export default {
+   components: { WxcSearchbar },
+   data: () => ({
+     value: ''
+   }),
+   methods: {
+     wxcSearchbarInputOnFocus () {
+     },
+     wxcSearchbarInputOnBlur () {
+     },
+     wxcSearchbarInputReturned (){
+     },
+     wxcSearchbarCloseClicked () {
+     },
+     wxcSearchbarInputOnInput (e) {
+       this.value = e.value;
+     },
+     wxcSearchbarCancelClicked () {
+     },
+     wxcSearchbarInputDisabledClicked () {
+     },
+     wxcSearchbarDepChooseClicked () {
+     }
+   }
+ }
 </script>
-
-<style scoped>
-  .wrapper {
-    /* flex-direction: column; */
-    justify-content: center;
-  }
-  .panel {
-    width: 750px;
-    background-color: #f2f3f4;
-    align-items: center;
-    justify-content: center;
-    margin-left: 0px;
-    border-width: 2px;
-    border-style: solid;
-    border-color: #BBBBBB;
-    padding-top: 15px;
-    padding-bottom: 15px;
-    padding-left: 15px;
-    padding-right: 15px;
-    margin-bottom: 30px;
-  }
-</style>
