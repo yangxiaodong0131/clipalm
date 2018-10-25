960db666d8bd20bdd1cf3bb74e830f2a94b62c9b
duanzhichao <duanzhichao2008@163.com>
2018-10-25 10:04:58 +0800

    加载更多测试

--- src/components/edit/Edit.vue
+++ src/components/edit/Edit.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="demo" @swipe="swipe">
+  <div class="demo" @swipe="swipe" style="height:1000px;">
     <text class="demo-title">{{wxcCellTitle}}</text>
-    <wxc-cell v-for="wt4 in wt4Case"
+    <!-- <wxc-cell v-for="wt4 in wt4Case"
               v-bind:key="wt4.id"
               :label="wt4.drg"
               @wxcCellClicked="wxcCellClicked(wt4)"
               :has-margin="false"
               :extraContent="wt4.extraContent"></wxc-cell>
-  </div>
+  </div> -->
+  <list class="list" @loadmore="fetch" loadmoreoffset="20">
+    <cell class="cell" v-for="num in wt4Case" v-bind:key="num.id">
+      <div class="panel">
+        <text class="text">{{num.extraContent}}</text>
+      </div>
+    </cell>
+  </list>
+</div>
 </template>
 
 <script>
 import { WxcRichText, WxcSpecialRichText, WxcPopup, WxcCell, WxcIndexlist, WxcLoading, WxcPartLoading } from 'weex-ui'
 import { getServer } from '../../utils/server'
 import { getDetails } from '../../utils/details'
+const modal = weex.requireModule('modal')
+const LOADMORE_COUNT = 4
 export default {
   components: { WxcIndexlist, WxcRichText, WxcSpecialRichText, WxcPopup, WxcCell, WxcLoading, WxcPartLoading },
   data () {
     return {
-      forceValue: 0
+      forceValue: 0,
+      refreshing: false
     }
   },
   created () {
@@ -63,50 +74,58 @@ export default {
         this.$store.commit('SET_menu', [this.$store.state.Home.activeTab, '病案详情'])
         this.$store.commit('SET_infoLevel', 1)
       }
+    },
+    fetch (event) {
+      modal.toast({ message: 'loadmore', duration: 1 })
+      setTimeout(() => {
+        const length = this.lists.length
+        for (let i = length; i < length + LOADMORE_COUNT; ++i) {
+          this.lists.push(i + 1)
+        }
+      }, 800)
     }
   }
 }
 </script>
 
 <style scoped>
-  .wxc-demo {
-    width: 750px;
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    background-color: #ffffff;
+  .refresh {
+    width: 750;
+    display: -ms-flex;
+    display: -webkit-flex;
+    display: flex;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    -webkit-box-align: center;
+    align-items: center;
   }
-  .container {
-    flex: 1;
+  .indicator-text {
+    color: #888888;
+    font-size: 42px;
+    text-align: center;
   }
-  .demo {
-    width: 750px;
-    background-color: #f2f3f4;
+  .indicator {
+    margin-top: 16px;
+    height: 40px;
+    width: 40px;
+    color: blue;
   }
-  .category {
-    margin-top: 40px;
+  .panel {
+    width: 600px;
+    height: 250px;
+    margin-left: 75px;
+    margin-top: 35px;
+    margin-bottom: 35px;
+    flex-direction: column;
+    justify-content: center;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #DDDDDD;
+    background-color: #F5F5F5;
   }
-  .default {
-    color: #000000;
-  }
-  .active {
-    color: #FFC900;
-  }
-  .red {
-    color: #C3413D;
-  }
-  .image {
-    width: 80px;
-    height: 80px;
-    margin-right: 20px;
-  }
-  .demo-title {
-    font-size: 30px;
-    color: #333333;
-    margin-top: 30px;
-    margin-left: 30px;
-    margin-bottom: 16px;
+  .text {
+    font-size: 50px;
+    text-align: center;
+    color: #41B883;
   }
 </style>
