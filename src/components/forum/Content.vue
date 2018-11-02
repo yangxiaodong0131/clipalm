739806c33e0c0ee4e2abcdab00e4d8356e1bf3ea
duanzhichao <duanzhichao2008@163.com>
2018-11-02 15:19:20 +0800

    修复帖子内容不显示的bug

--- src/components/forum/Content.vue
+++ src/components/forum/Content.vue
@@ -1,14 +1,16 @@
 <template>
-  <!-- <div class="demo">
-      <cell class="cell" v-for="(data, index) in datas" v-bind:key="index">
-        <text>132456789123456789</text>
-        <wxc-cell
-          :label="data.username"
-          :title="data.content"
-          :extraContent="data.datetime"
-          :has-arrow="false"
-          ></wxc-cell>
-      </cell>
+  <div class="demo">
+      <list class="list" loadmoreoffset="20">
+        <cell class="cell" v-for="(data, index) in datas" v-bind:key="index">
+          <wxc-cell
+            :label="data.username"
+            :title="data.content"
+            :extraContent="data.datetime"
+            :has-margin="false"
+            @wxcCellClicked="wxcCellClicked(wt4)">
+          </wxc-cell>
+        </cell>
+      </list>
       <div class="wrapper">
         <textarea class="textarea" placeholder="输入帖子内容" @input="oninput2"></textarea>
       </div>
@@ -16,9 +18,6 @@
         class="submits"
         size="full"
         @wxcButtonClicked="wxcButtonClicked"></wxc-button>
-  </div> -->
-  <div>
-    <div class="box"></div>
   </div>
 </template>
 <script>
@@ -34,7 +33,6 @@ export default {
   },
   computed: {
     datas () {
-      modal.toast({ message: '请先登录', duration: 1 })
       return this.$store.state.Forum.forumContent.content
     }
   },
@@ -54,31 +52,18 @@ export default {
 }
 </script>
 <style scoped>
-  .demo {
-    width: 750px;
-    background-color: #f2f3f4;
-    margin-top: 192px;
-  }
-  .submits{
-    position: relative;
-    margin-top: 20px;
-    left: 23px;
-    top: 1px;
-  }
-  .cell{
-    margin-top: 91px;
-  }
-  .wrapper{
-    margin-top: 20px;
-  }
-  .box {
-    border-width: 2px;
-    border-style: solid;
-    border-color: #BBB;
-    width: 250px;
-    height: 250px;
-    margin-top: 250px;
-    margin-left: 250px;
-    background-color: #EEE;
-  }
+.demo-title {
+  font-size: 28px;
+  background-color: #C6E2FF;
+  text-align: center;
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 14px;
+  padding: 10px;
+}
+.demo {
+  width: 750px;
+  height: 1250px;
+  margin-top: 91px;
+}
 </style>
