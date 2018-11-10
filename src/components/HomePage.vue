119b8540fbee5aff905b1b309d791239363cc894
duanzhichao <duanzhichao2008@163.com>
2018-11-10 11:05:16 +0800

    删除无用代码

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -20,21 +20,21 @@
     <div class="panel" v-bind:class="panel">
       <PopRight v-if="infoLevel > 0"></PopRight>
       <HomeMenu v-else-if="menu[1] === '菜单'"></HomeMenu>
-      <Edit v-else-if="['未入组病历', '低风险死亡病历', '费用异常病历', 'QY病历'].includes(menu[1])"></Edit>
       <SingleGroup v-else-if="menu[1] == '单条分组'"></SingleGroup>
+      <Edit v-else></Edit>
     </div>
     <!-- library页 -->
     <div class="panel" v-bind:class="panel">
       <PopRight v-if="infoLevel > 0"></PopRight>
       <HomeMenu v-else-if="menu[2] === '菜单'"></HomeMenu>
-      <Library v-else-if="['MDC', 'ADRG', 'DRG', 'ICD10', 'ICD9'].includes(menu[2])"></Library>
+      <Library v-else></Library>
     </div>
     <!-- stat页 -->
     <div class="panel" v-bind:class="panel">
       <PopRight v-if="infoLevel > 0"></PopRight>
       <HomeMenu v-else-if="menu[3] === '菜单'"></HomeMenu>
-      <Report v-else-if="['统计分析(字母增序)', '统计分析(费用CV降序)', '统计分析(平均费用增序)'].includes(menu[3])"></Report>
       <Charts v-else-if="menu[3] == '报表'"></Charts>
+      <Report v-else></Report>
     </div>
     <!-- forum页 -->
     <div class="panel" v-bind:class="panel">
@@ -72,8 +72,6 @@
     components: { WxcTabBar, WxcLoading, User, Login, Edit, SingleGroup, Library,
       Report, Forum, PopRight, MiniBar, ForumContent, Version, Charts, New, Register, HomeMenu },
     data: () => ({
-      // icon: 'https://gw.alicdn.com/tfs/TB1MWXdSpXXXXcmXXXXXXXXXXXX-72-72.png',
-      // activeIcon: 'https://gw.alicdn.com/tfs/TB1kCk2SXXXXXXFXFXXXXXXXXXX-72-72.png'
       tabs: [{
         title: '用户',
         menu:  ['用户登录', '个人信息'],
@@ -113,26 +111,14 @@
         fontSize: 24,
         textPaddingLeft: 10,
         textPaddingRight: 10
-      },
-      test: ''
+      }
     }),
     computed: {
       infoLevel () {
         return this.$store.state.Home.infoLevel
       },
       menu () {
-        console.log(this.$store.state.Home.menu)
         return this.$store.state.Home.menu
-      },
-      showForum () {
-        return this.$store.state.Home.showForum
-      },
-      isShow () {
-        let isShow = false
-        if (this.$store.state.Home.activeTab > 0) {
-          isShow = true
-        }
-        return isShow
       },
       isLoadingShow () {
         return this.$store.state.Home.isLoadingShow
@@ -179,34 +165,8 @@
       const { tabStyles } = this
       this.contentStyle = { height: (tabPageHeight - tabStyles.height) + 'px' }
       this.newVersion()
-      // storage.getItem('point', e => {
-      //   if (e.result === 'success') {
-      //     this.$store.commit('SET_pointIndex', parseInt(e.data))
-      //   } else {
-      //     this.$store.commit('SET_pointIndex', 0)
-      //   }
-      // })
     },
     beforeMount: function () {
-      // this.$store.commit('SET_activeTab', 0)
-      // const point = this.$store.state.Home.point
-      // const pointIndex = this.$store.state.Home.pointIndex
-      // if (pointIndex === point.length - 1) {
-      //   modal.confirm({ 'message': `Tips: ${point[pointIndex]}`, 'duration': 5, 'cancelTitle': '重新显示提示', 'okTitle': '我知道了' }, function (value) {
-      //     if (value === '重新显示提示') {
-      //       storage.setItem('point', 0)
-      //     } else {
-      //       storage.setItem('point', pointIndex + 1)
-      //     }
-      //   })
-      // } else if (pointIndex <= point.length) {
-      //   modal.toast({ 'message': `${pointIndex}`, 'duration': 0.3 })
-      //   modal.alert({ 'message': `Tips: ${point[pointIndex]}`, 'duration': 5, 'okTitle': '我知道了' }, function () {
-      //     storage.setItem('point', pointIndex + 1, e => {
-      //       modal.toast({ 'message': `${e.result}`, 'duration': 0.3 })
-      //     })
-      //   })
-      // }
     },
     mounted: function () {
       this.wxcTabBarCurrentTabSelected({ page: 0 })
@@ -225,64 +185,9 @@
         const i = e.page
         const menus = this.tabs[i].menu
         const menu = this.$store.state.Home.menu[i]
-        // const activeMenu = this.$store.state.Home.activeMenu[i]
-        // let menu = menus[activeMenu]
-        // // 解决多页面问题
-        // if (menu === -1) {
-        //   menu = '菜单'
-        // }
         this.$store.commit('SET_activeTab', i)
         this.$store.commit('SET_menus', menus)
         this.$store.commit('SET_menu', [i, menu])
-        // const menus = this.tabs[i]['menu']
-        // const activeMenu = this.$store.state.Home.activeMenu[i]
-        // this.$store.commit('SET_activeTab', i)
-        // this.$store.commit('SET_menus', this.tabs[i]['menu'])
-        // this.$store.commit('SET_menu', [i, menus[activeMenu]])
-
-        // let menu = ''
-        // let miniBarTitle = ''
-        // switch (i) {
-        //   case 0:
-        //     menu = this.$store.state.Home.userMenu
-        //     miniBarTitle = this.tabs[e.page]['title']
-        //     break
-        //   case 1:
-        //     menu = this.$store.state.Edit.editMenu
-        //     miniBarTitle = this.tabs[e.page]['title']
-        //     break
-        //   case 2:
-        //     menu = this.$store.state.Library.libraryMenu
-        //     miniBarTitle = this.tabs[e.page]['title']
-        //     break
-        //   case 3:
-        //     menu = this.$store.state.Stat.statMenu
-        //     miniBarTitle = this.tabs[e.page]['title']
-        //     break
-        //   case 4:
-        //     if (this.$store.state.Home.activeTab !== 4) {
-        //       this.$store.commit('SET_forumHead', this.$store.state.Home.miniBarTitle)
-        //       miniBarTitle = `${this.$store.state.Forum.forumHead}-帖子`
-        //       this.$store.commit('SET_forumLabel', `${this.$store.state.Home.miniBarTitle}`)
-        //       menu = '论坛'
-        //     } else {
-        //       miniBarTitle = this.$store.state.Home.miniBarTitle
-        //       menu = `${this.$store.state.Forum.forumMenu}`
-        //     }
-        //     getServer(this, 'all', menu)
-        //     this.$store.commit('SET_forumMenu', menu)
-        //     break
-        // }
-        // this.$store.commit('SET_menu', [i, menu])
-        // if (menu != '' && i !== 4) {
-        //   miniBarTitle = menu
-        // }
-        // this.$store.commit('SET_miniBarTitle', miniBarTitle)
-        // // 设定loading查询隐藏
-        // if (i !== this.$store.state.Home.activeTab && this.$store.state.Home.isLoadingShow == true) {
-        //   this.$store.commit('SET_isLoadingShow', false)
-        // }
-        // this.$store.commit('SET_activeTab', i)
       }
     }
   };
