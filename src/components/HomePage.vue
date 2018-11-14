0aea913fd06e8b9352d06f2360fe366e9f50fa9a
yangxiaodong <1072702569@qq.com>
2018-11-14 08:48:05 +0800

    修改字典菜单页

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -87,7 +87,7 @@
           activeIcon: 'http://210.75.199.113/images/edit_fill.png'
         }, {
           title: '字典',
-          menu: ['MDC', 'ADRG', 'DRG', 'ICD10', 'ICD9'],
+          menu: ['CN-DRG', 'BJ-ICD10', 'BJ-ICD9', 'GB-ICD10', 'GB-ICD9'],
           icon: 'http://210.75.199.113/images/library.png',
           activeIcon: 'http://210.75.199.113/images/library_fill.png'
         }, {
