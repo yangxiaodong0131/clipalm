a353252fde36e5498adb946f793df8f3bebd6eec
duanzhichao <duanzhichao2008@163.com>
2018-11-21 12:54:05 +0800

    版本bug修复

--- src/components/edit/Edit.vue
+++ src/components/edit/Edit.vue
@@ -12,7 +12,7 @@
       <cell v-for="(wt4, index) in wt4Case" v-bind:key="index" @longpress="test">
         <div class="panel" @longpress="longpress(wt4)">
           <wxc-cell
-            :title="wt4.disease_name"
+            :title="wt4.disease_code"
             :desc="wt4.extraContent"
             :has-margin="false"
             :has-arrow="true"
