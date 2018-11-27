3dcdfdffc42dd80c474903d48829e12dc4609208
tanyawen0602 <1473213664@qq.com>
2018-11-27 15:19:40 +0800

    病案增加菜单按钮

--- src/utils/server.js
+++ src/utils/server.js
@@ -56,6 +56,14 @@ export function getServer (obj, activeTab, menu, value = null) {
         url = `wt4_2017?plat=client&cv=1&page=${obj.$store.state.Edit.wt4Page}&version=${version}`
         router = 'drgwork_wt4/expert'
         break
+      case '填报异常病历':
+        url = `wt4_2017?plat=client&bug=1&page=${obj.$store.state.Edit.wt4Page}&version=${version}`
+        router = 'drgwork_wt4/expert'
+        break
+      case '我的病案':
+        url = `my_wt4?plat=client&cv=1&page=${obj.$store.state.Edit.wt4Page}&version=${version}`
+        router = 'drgwork_wt4/expert'
+        break
       case 'CN-DRG':
         url = `rule_bj_mdc?plat=client&version=${version}&year=${year}&page=${obj.$store.state.Library.page}`
         break
