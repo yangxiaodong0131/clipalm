a0752446bc395f1e3276d7b803ed64fe4125e950
yangxiaodong <1072702569@qq.com>
2018-11-28 11:12:05 +0800

    增加完善个人信息功能

--- src/utils/server.js
+++ src/utils/server.js
@@ -95,6 +95,17 @@ export function getServer (obj, activeTab, menu, value = null) {
       case 'DRG基础':
         url = `wt4_stat_mdc?plat=client&order=code&page=${obj.$store.state.Stat.statPage}`
         break
+      case '主诊未入组':
+        url = `wt4_stat_cv_disease?plat=client&version=${version}&page=${obj.$store.state.Stat.statPage}`
+        router = 'drgwork_wt4/expert'
+        break
+      case '手术QY':
+        url = `wt4_stat_cv_qy?plat=client&version=${version}&page=${obj.$store.state.Stat.statPage}`
+        router = 'drgwork_wt4/expert'
+        break
+      case '偏差分布':
+        url = `wt4_stat_mdc?plat=client&order=code&page=${obj.$store.state.Stat.statPage}`
+        break
       case 'DRG机构-年':
         url = `wt4_stat_cv_org?type=year&plat=client&version=${version}&page=${obj.$store.state.Stat.statPage}`
         router = 'drgwork_wt4/expert'
@@ -109,18 +120,6 @@ export function getServer (obj, activeTab, menu, value = null) {
         break
       case 'DRG机构-月':
         url = `wt4_stat_cv_org?type=month&plat=client&version=${version}&page=${obj.$store.state.Stat.statPage}`
-        router = 'drgwork_wt4/expert'
-        break
-      case '主诊未入组':
-        url = `wt4_stat_cv_disease?plat=client&version=${version}&page=${obj.$store.state.Stat.statPage}`
-        router = 'drgwork_wt4/expert'
-        break
-      case '手术QY':
-        url = `wt4_stat_cv_qy?plat=client&version=${version}&page=${obj.$store.state.Stat.statPage}`
-        router = 'drgwork_wt4/expert'
-        break
-      case '偏差分布':
-        url = `wt4_stat_cv_dd?plat=client&version=${version}&page=${obj.$store.state.Stat.statPage}`
         router = 'drgwork_wt4/expert'
         break
       case '贴子列表':
