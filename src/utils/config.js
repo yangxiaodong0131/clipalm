83c4e7d99f4ebe35c51e76d41a326f4bc4498238
duanzhichao <duanzhichao2008@163.com>
2018-12-01 15:14:13 +0800

    修复bug

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,26 +1,26 @@
 // // 开发配置
 // module.exports = {
-//   http: 'http://192.168.99.161',
+//   http: 'http://192.168.99.25',
 //   static: 'http://www.jiankanglaifu.com',
 //   drgserver: 'http://192.168.0.79',
 //   port: '3009',
 //   router: 'expert'
 // }
 
-// 开发配置
-module.exports = {
-  http: 'http://192.168.0.79',
-  static: 'http://www.jiankanglaifu.com',
-  drgserver: 'http://192.168.0.79',
-  port: '3009',
-  router: 'expert'
-}
-
-// // 服务器配置
+// // 开发配置
 // module.exports = {
-//   http: 'http://www.jiankanglaifu.com',
+//   http: 'http://192.168.0.79',
 //   static: 'http://www.jiankanglaifu.com',
-//   drgserver: 'http://139.129.165.56',
-//   port: '80',
-//   router: 'drgwork/expert'
+//   drgserver: 'http://192.168.0.79',
+//   port: '3009',
+//   router: 'expert'
 // }
+
+// 服务器配置
+module.exports = {
+  http: 'http://www.jiankanglaifu.com',
+  static: 'http://www.jiankanglaifu.com',
+  drgserver: 'http://139.129.165.56',
+  port: '80',
+  router: 'drgwork/expert'
+}
